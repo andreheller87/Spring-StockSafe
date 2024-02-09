ec0a0f71740d0b32dfbb12dc1063cbea717e7603
andreheller87 <andreheller02@gmail.com>
2024-02-09 18:30:32 -0300

    Produto

--- course/src/main/java/com/stocksafe/course/resource/ProdutoResource.java
+++ course/src/main/java/com/stocksafe/course/resource/ProdutoResource.java
@@ -1,13 +1,19 @@
 package com.stocksafe.course.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.stocksafe.course.entities.Produto;
 import com.stocksafe.course.services.ProdutoService;
@@ -29,5 +35,24 @@ public class ProdutoResource {
 	public ResponseEntity<Produto> findById(@PathVariable Long id) {
 		Produto product = produtoService.findById(id);
 		return ResponseEntity.ok().body(product);
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> insert(@RequestBody Produto obj) {
+		obj = produtoService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		produtoService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto obj) {
+		obj = produtoService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- course/src/main/java/com/stocksafe/course/services/ProdutoService.java
+++ course/src/main/java/com/stocksafe/course/services/ProdutoService.java
@@ -9,20 +9,43 @@ import org.springframework.stereotype.Service;
 import com.stocksafe.course.entities.Produto;
 import com.stocksafe.course.repositories.ProdutoRepository;
 
-
-
 @Service
 public class ProdutoService {
 
-    @Autowired
-    private ProdutoRepository produtoRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
-    public List<Produto> findAll() {
-        return produtoRepository.findAll();
-    }
+	public List<Produto> findAll() {
+		return produtoRepository.findAll();
+	}
 
-    public Produto findById(Long id) {
-        Optional<Produto> obj = produtoRepository.findById(id);
-        return obj.orElse(null);
-    }
+	public Produto findById(Long id) {
+		Optional<Produto> obj = produtoRepository.findById(id);
+		return obj.orElse(null);
+	}
+
+	public Produto insert(Produto obj) {
+		return produtoRepository.save(obj);
+	}
+
+	public void delete(Long id) {
+		produtoRepository.deleteById(id);
+	}
+
+	public Produto update(Long id, Produto obj) {
+		Produto entity = produtoRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return produtoRepository.save(entity);
+	}
+
+	private void updateData(Produto entity, Produto obj) {
+		entity.setCodLote(obj.getCodLote());
+		entity.setNome(obj.getNome());
+		entity.setValor(obj.getValor());
+		entity.setData(obj.getData());
+		entity.setValidade(obj.getValidade());
+		entity.setQuantidade(obj.getQuantidade());
+		entity.setObservacao(obj.getObservacao());
+		entity.setArmazenamento(obj.getArmazenamento());
+	}
 }
