e641b7d4b73b98f3011a6f26e27963f0bbfbd3e3
andreheller87 <andreheller02@gmail.com>
2024-02-09 15:35:16 -0300

    Usuario por id

--- course/src/main/java/com/stocksafe/course/resource/UserResource.java
+++ course/src/main/java/com/stocksafe/course/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +13,23 @@ import com.stocksafe.course.entities.Usuario;
 import com.stocksafe.course.services.UserService;
 
 @RestController
-@RequestMapping(value= "/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	    @GetMapping
-		public ResponseEntity<List<Usuario>> findAll(){
-		
-	    	List<Usuario> use = userService.findAll();
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAll() {
+
+		List<Usuario> use = userService.findAll();
 		return ResponseEntity.ok().body(use);
-		}
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+
+		Usuario use = userService.findById(id);
+		return ResponseEntity.ok().body(use);
+	}
 }
--- course/src/main/java/com/stocksafe/course/services/UserService.java
+++ course/src/main/java/com/stocksafe/course/services/UserService.java
@@ -1,6 +1,7 @@
 package com.stocksafe.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,4 +18,11 @@ public class UserService {
 	public List<Usuario> findAll() {
 		return userRepository.findAll();
 	}
+
+	public Usuario findById(Long id) {
+		Optional<Usuario> obj = userRepository.findById(id);
+		return obj.get();
+
+	}
+
 }
